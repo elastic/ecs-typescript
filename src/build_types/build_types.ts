9e1b77bc21bd3551274773b343fa53f13729ed4d
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-01-26 15:15:14 -0700

    Adds helper methods for building interfaces

--- src/build_types/build_types.ts
+++ src/build_types/build_types.ts
@@ -1,10 +1,13 @@
 import { convertType } from './convert_type';
+import { Helpers} from './helpers';
+
+const helper = new Helpers();
 
 export function buildTypes(spec: Record<string, any>): string {
-  const description = spec?.agent?.description.split('\n').map((line: string) => `// ${line}`);
+  const description = helper.buildDescription(spec?.agent?.description);
   return `// ECS types
 
-${description.join('\n')}
+${description}
 export interface Test {
   ${spec.agent.fields['agent.name'].name}?: ${convertType(spec.agent.fields['agent.name'].type)};
 }
