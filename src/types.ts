bd47f85aad963f06bd465c3ff7a9c8d55f849da3
Luke Elmers <luke.elmers@elastic.co>
2022-01-28 14:48:02 -0700

    Add types for ECS nested spec. (#5)

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+/**
+ * Represents the top-level structure of the ecs_nested.yml.
+ * 
+ * This is not a full representation of the spec, just some
+ * of the fields we care about.
+ */
+export type EcsNestedSpec = Record<string, EcsGroupSpec>;
+
+/**
+ * Metadata for each of the top-level items in the EcsSpec.
+ */
+export interface EcsGroupSpec {
+  description: string;
+  fields: Record<string, EcsFieldSpec>;
+  footnote?: string;
+  group: number;
+  prefix: string;
+  root?: boolean;
+  short: string;
+  title: string;
+  type: string;
+}
+
+/**
+ * Metadata for each field in a EcsGroupSpec.
+ */
+export interface EcsFieldSpec {
+  dashed_name: string;
+  description: string;
+  example: string;
+  flat_name: string;
+  ignore_above: number
+  level: 'core' | 'extended';
+  name: string;
+  normalize: string[];
+  required?: boolean;
+  short: string;
+  type: string; // TODO: include string literals
+}
