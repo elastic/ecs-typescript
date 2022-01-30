49cd0c45d1e1c16064cfa10dbef0ec9110d359e9
Christiane Heiligers <christiane.heiligers@elastic.co>
2022-01-29 18:25:48 -0700

    adds helper for creating interface test fixtures

--- src/build_types/interface.test.ts
+++ src/build_types/interface.test.ts
@@ -2,6 +2,58 @@
 import { EcsFieldSpec } from '../types';
 import { Interface } from './interface';
 
+/**
+ * utility to create a test fixture with the following shape:
+ * interface A {
+   prop_a?: 'keyword';
+   prop_b?: PropB;
+ }
+ interface PropB {
+   prop_c: 'keyword';
+   prop_d?: PropD;
+ }
+ interface PropD {
+   prop_e?: 'keyword';
+   prop_f?: 'keyword';
+ }
+ */
+function createInterfaceWithProps(): Interface {
+  const interfacesMeta = [
+    { name: "A", description: "Description of A" },
+    { name: "PropB", description: "Description of PropB" },
+    { name: "PropD", description: "Description of PropD" },
+  ];
+  const defaultFieldProps = (
+    name: string,
+    type = "keyword",
+    required = false
+  ): EcsFieldSpec => ({
+    dashed_name: `dash-${name}`,
+    description: "test_prop description",
+    example: "example",
+    flat_name: `flat_${name}`,
+    level: "core",
+    name: `${name}`,
+    normalize: [],
+    required,
+    short: "short description",
+    type,
+    ignore_above: 1024,
+  });
+  const intfs = interfacesMeta.map(
+    (_, index) => new Interface(interfacesMeta[index])
+  );
+  intfs[0].addProperty("prop_a", defaultFieldProps("prop_a"));
+  intfs[0].addProperty("prop_b", intfs[1]);
+
+  intfs[1].addProperty("prop_c", defaultFieldProps("prop_c"));
+  intfs[1].addProperty("prop_d", intfs[2]);
+
+  intfs[2].addProperty("prop_e", defaultFieldProps("prop_e"));
+  intfs[2].addProperty("prop_f", defaultFieldProps("prop_f"));
+  return intfs[0];
+}
+
 describe('Interface', () => {
   let mainInt: Interface;
   let testProp: EcsFieldSpec;
@@ -125,55 +177,27 @@ test_prop?: string;
 "
 `)
   });
-  it('writes all dedicated interfaces', () => {
-    const cloudInterface = new Interface({ name: 'cloud', description: 'cloud description' });
-    const availabilityZone: EcsFieldSpec = {
-      dashed_name: 'cloud-availability-zone',
-      description: 'Availability zone in which this host, ',
-      example: 'fus-east-1coo',
-      flat_name: 'cloud.availability_zone',
-      level: 'extended',
-      name: 'availability_zone',
-      normalize: [],
-      required: false,
-      short: 'Availability zone in which this host, resource, or service is located.',
-      type: 'keyword',
-      ignore_above: 1024
-    };
-    const accountInterface = new Interface({ name: 'account', description: 'account description'});
-    const accountInterfaceProps: EcsFieldSpec = {
-      dashed_name: 'cloud-account-id',
-      description: 'The cloud account or organization id used to identify different\n entities in a multi-tenant environment.',
-      example: '666777888999',
-      flat_name: 'cloud.account.id',
-      level: 'extended',
-      name: 'id',
-      normalize: [],
-      required: false,
-      short: 'The cloud account or organization id.',
-      type: 'keyword',
-      ignore_above: 1024
-    };
-    accountInterface.addProperty(accountInterfaceProps.name, accountInterfaceProps);
-    cloudInterface.addProperty(availabilityZone.name, availabilityZone);
-    cloudInterface.addProperty(accountInterface.name, accountInterface);
-    
-    expect(cloudInterface.properties).toStrictEqual(expect.objectContaining({
-      availability_zone: expect.any(Object),
-      account: expect.any(Interface),
-    }));
-    expect(cloudInterface.toInterfaceString(true)).toMatchInlineSnapshot(`
-"// cloud description
-    export interface EcsCloud {
-availability_zone: string;
-account?: Account;
+  
+  it("writes all dedicated interfaces", () => {
+    const testInterface = createInterfaceWithProps();
+    expect(testInterface.toInterfaceString(true)).toMatchInlineSnapshot(`
+"// Description of A
+    export interface EcsA {
+prop_a: string;
+PropB?: PropB;
 }
-// account description
-    interface Account {
-id: string;
+// Description of PropB
+    interface PropB {
+prop_c: string;
+PropD?: PropD;
+}
+// Description of PropD
+    interface PropD {
+prop_e: string;
+prop_f: string;
 }
 "
-`)
+`);
   });
 });
 
