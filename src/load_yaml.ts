7779464a800e4e8e4191aa1358e6385a17030527
Luke Gmys <lgmys@users.noreply.github.com>
2022-12-21 15:51:01 +0100

    Introduce GitHub actions (#15)

--- src/load_yaml.ts
+++ src/load_yaml.ts
@@ -1,15 +1,21 @@
-import path from 'path';
-import fs from 'fs';
 import { load } from 'js-yaml';
 
-export function loadYaml(specPath: string) {
+import axios from 'axios';
+
+export async function loadYaml(ref: string) {
+  const url = `https://raw.githubusercontent.com/elastic/ecs/${ref}/generated/ecs/ecs_nested.yml`;
+
+  const response = await axios.get<string>(url);
+
+  if (response.status !== 200) {
+    throw new Error(`Could not fetch ecs spec from ${url}`);
+  }
+
   try {
-    const doc = load(
-      fs.readFileSync(path.resolve(__dirname, '..', '..', specPath), 'utf8')
-    );
+    const doc = load(response.data);
     return doc as Record<string, any>;
   } catch (e) {
-    console.error(`Failed to load spec from ${specPath}`);
+    console.error(`Failed to load spec from ${url}`);
     console.error(e);
     process.exit(1);
   }
