a326709172f4f67484167a135f31e45c7d892849
lgestc <lgestc@users.noreply.github.com>
2023-04-12 15:19:39 +0000

    [create-pull-request] automated change

--- generated/elf.ts
+++ generated/elf.ts
@@ -41,6 +41,27 @@ export interface EcsElf {
    * List of exported element names and types.
    */
   exports?: Record<string, unknown> | Array<Record<string, unknown>>;
+  /**
+   * A hash of the Go language imports in an ELF file excluding standard library imports. An import hash can be used to fingerprint binaries even after recompilation or other code-level transformations have occurred, which would change more traditional hash values.
+   * The algorithm used to calculate the Go symbol hash and a reference implementation are available [here](https://github.com/elastic/toutoumomoma).
+   */
+  go_import_hash?: string;
+  /**
+   * List of imported Go language element names and types.
+   */
+  go_imports?: Record<string, unknown>;
+  /**
+   * Shannon entropy calculation from the list of Go imports.
+   */
+  go_imports_names_entropy?: number;
+  /**
+   * Variance for Shannon entropy calculation from the list of Go imports.
+   */
+  go_imports_names_var_entropy?: number;
+  /**
+   * Set to true if the file is a Go executable that has had its symbols stripped or obfuscated and false if an unobfuscated Go executable.
+   */
+  go_stripped?: boolean;
   header?: {
     /**
      * Version of the ELF Application Binary Interface (ABI).
@@ -77,9 +98,22 @@ export interface EcsElf {
   };
 
   /**
+   * A hash of the imports in an ELF file. An import hash can be used to fingerprint binaries even after recompilation or other code-level transformations have occurred, which would change more traditional hash values.
+   * This is an ELF implementation of the Windows PE imphash.
+   */
+  import_hash?: string;
+  /**
    * List of imported element names and types.
    */
   imports?: Record<string, unknown> | Array<Record<string, unknown>>;
+  /**
+   * Shannon entropy calculation from the list of imported element names and types.
+   */
+  imports_names_entropy?: number;
+  /**
+   * Variance for Shannon entropy calculation from the list of imported element names and types.
+   */
+  imports_names_var_entropy?: number;
   /**
    * An array containing an object for each section of the ELF file.
    * The keys that should be present in these objects are defined by sub-fields underneath `elf.sections.*`.
---
+++ generated/macho.ts
@@ -0,0 +1,72 @@
+/*
+ * Licensed to Elasticsearch B.V. under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * These fields contain Mac OS Mach Object file format (Mach-O) metadata.
+ */
+export interface EcsMacho {
+  /**
+   * A hash of the Go language imports in a Mach-O file excluding standard library imports. An import hash can be used to fingerprint binaries even after recompilation or other code-level transformations have occurred, which would change more traditional hash values.
+   * The algorithm used to calculate the Go symbol hash and a reference implementation are available [here](https://github.com/elastic/toutoumomoma).
+   */
+  go_import_hash?: string;
+  /**
+   * List of imported Go language element names and types.
+   */
+  go_imports?: Record<string, unknown>;
+  /**
+   * Shannon entropy calculation from the list of Go imports.
+   */
+  go_imports_names_entropy?: number;
+  /**
+   * Variance for Shannon entropy calculation from the list of Go imports.
+   */
+  go_imports_names_var_entropy?: number;
+  /**
+   * Set to true if the file is a Go executable that has had its symbols stripped or obfuscated and false if an unobfuscated Go executable.
+   */
+  go_stripped?: boolean;
+  /**
+   * A hash of the imports in a Mach-O file. An import hash can be used to fingerprint binaries even after recompilation or other code-level transformations have occurred, which would change more traditional hash values.
+   * This is a synonym for symhash.
+   */
+  import_hash?: string;
+  /**
+   * List of imported element names and types.
+   */
+  imports?: Record<string, unknown> | Array<Record<string, unknown>>;
+  /**
+   * Shannon entropy calculation from the list of imported element names and types.
+   */
+  imports_names_entropy?: number;
+  /**
+   * Variance for Shannon entropy calculation from the list of imported element names and types.
+   */
+  imports_names_var_entropy?: number;
+  /**
+   * An array containing an object for each section of the Mach-O file.
+   * The keys that should be present in these objects are defined by sub-fields underneath `macho.sections.*`.
+   */
+  sections?: Record<string, unknown> | Array<Record<string, unknown>>;
+  /**
+   * A hash of the imports in a Mach-O file. An import hash can be used to fingerprint binaries even after recompilation or other code-level transformations have occurred, which would change more traditional hash values.
+   * This is a Mach-O implementation of the Windows PE imphash
+   */
+  symhash?: string;
+}
